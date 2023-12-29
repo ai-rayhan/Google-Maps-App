a6ffae446f8b3ef9973bbf974150b258375d5e23
RayhanAhmed <rayhanahmed1127@gmail.com>
2023-12-29 23:06:14 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:googlemapsapp/map_screen.dart';
+
+void main() {
+  runApp(const GoogleMapsApp());
+}
+
+class GoogleMapsApp extends StatelessWidget {
+  const GoogleMapsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home:  MapScreen(),
+    );
+  }
+}
